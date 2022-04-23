8487c9c65cd5862a908ba4db1707785f3ef89876
KemiScog <kemiscoggins@comcast.net>
2022-04-22 20:47:20 -0400

    disable input ablilty for set sudoku values. (can't edit the orignal puzzle)

--- app/src/main/java/com/schiesh/sudoku/MainActivity.java
+++ app/src/main/java/com/schiesh/sudoku/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.GradientDrawable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
@@ -254,6 +255,11 @@ public class MainActivity extends AppCompatActivity {
                     myButton.setText("");
                 }else{
                     myButton.setText(String.valueOf(sudoku.getValue(i, j)));
+                    myButton.setFocusable(false);
+                    myButton.setEnabled(false);
+                    myButton.setCursorVisible(false);
+                    myButton.setKeyListener(null);
+                    myButton.setInputType(InputType.TYPE_NULL);
                 }
                 btnInt++;
             }
